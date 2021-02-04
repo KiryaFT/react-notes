6708b8a65a3507626d1d4c3fff7a8ff2014afbf0
Kirill <uper99@gmail.com>
2021-02-05 01:12:00 +0300

    Added files

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Мое React приложение</h1>
+            <p className="lead">Версия приложения <strong>1.0.1</strong></p>
+            <p className="lead">Made by <strong>KiryaFT</strong></p>
+        </div>
+    </div>
+)
